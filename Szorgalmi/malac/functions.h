d26857d28b24b4e763ec849426aa08c9661fdd28
bardoandris <bardoandris07@gmail.com>
2021-10-13 21:38:02 +0200

    A váz kész, jöhet a debugging :D
    Naggyon nem jó értéket add vissza a függvény, lehet rosszul írtam át papírról...

--- Szorgalmi/malac/functions.h
+++ Szorgalmi/malac/functions.h
@@ -1,8 +1,15 @@
 typedef struct zoldseg{
-	float so, szen, feh, ar;
+	float so, szen, feh, ar, menny;
 } zoldseg;
+
+typedef struct igeny{
+	float szen, feh, so; int menny;
+}igeny;
 
 zoldseg beker();
 float hatarertek(char *fajta);
 void initzoldseg(zoldseg *z1, zoldseg *z2);
 void inithatar(float *sohat,float *fehhat,float *szenhat, int *menny);
+float z1_megoldas(zoldseg z1, zoldseg z2, igeny ig);
+float z2_megoldas(zoldseg z1, zoldseg z2, igeny ig);
+int so_ellenor(zoldseg z1, zoldseg z2, igeny ig);
